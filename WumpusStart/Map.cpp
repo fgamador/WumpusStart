d4faf01e8a36be6a6e09504e7d88365746fb948a
fgamador <franzamador@gmail.com>
2016-03-14 09:44:43 -0700

    Update to match new refactoring in dev blog post

--- WumpusStart/Map.cpp
+++ WumpusStart/Map.cpp
@@ -1,11 +1,6 @@
 #include "Map.h"
 
-Map::Map()
-{
-    m_connected[2] = { 1, 3, 10 };
-}
-
 ints3 Map::GetConnectedRooms(int room)
 {
-    return m_connected[room];
+    return { 1, 3, 10 };
 }
--- WumpusStart/Map.h
+++ WumpusStart/Map.h
@@ -5,9 +5,5 @@
 class Map
 {
 public:
-    Map();
     ints3 GetConnectedRooms(int room);
-
-private:
-    array<ints3, 21> m_connected;
 };
